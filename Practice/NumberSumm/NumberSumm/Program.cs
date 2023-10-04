ae7a140036084de8f4f1d28278d5f316eed201c4
Новиков Никита <novikov.nik.job@yandex.ru>
2023-10-04 16:40:28 +0300

    Добавил папку с практикой и решение первой задачи

---
+++ Practice/NumberSumm/NumberSumm/Program.cs
@@ -0,0 +1,29 @@
+﻿public class Programm
+{
+    public static TextReader reader = new StreamReader(Console.OpenStandardInput());
+    public static TextWriter writer = new StreamWriter(Console.OpenStandardOutput());
+
+    public static void Main(string[] args)
+    {
+        var a = ReadInteger();
+        var b = ReadInteger();
+
+        writer.WriteLine(a + b);
+
+        reader.Close();
+        writer.Close();
+    }
+
+    public static int ReadInteger()
+    {
+        if (int.TryParse(reader.ReadLine(), out var num))
+        {
+            return num;
+        }
+        else
+        {
+            writer.WriteLine("Введенный аргумент имеет неверный формат.");
+            throw new ArgumentException("Введенный аргумент имеет неверный формат.");
+        }
+    }
+}
