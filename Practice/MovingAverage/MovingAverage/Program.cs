691abb7f4dec95959bd7be98f2e7656a84be17e1
Новиков Никита <novikov.nik.job@yandex.ru>
2023-10-04 22:13:41 +0300

    +

---
+++ Practice/MovingAverage/MovingAverage/Program.cs
@@ -0,0 +1,44 @@
+﻿public class Programm
+{
+    public static TextReader reader = new StreamReader(Console.OpenStandardInput());
+    public static TextWriter writer = new StreamWriter(Console.OpenStandardOutput());
+
+    public static void Main(string[] args)
+    {
+        var n = int.Parse(reader.ReadLine());
+        var resultStr = GetMovingAverage(reader.ReadLine(),
+            int.Parse(reader.ReadLine()));
+
+        writer.WriteLine(resultStr);
+
+        reader.Close();
+        writer.Close();
+    }
+
+    public static string GetMovingAverage(string arr,
+        int k)
+    {
+        var counter = 0;
+        var result = "";
+        var nums = new List<int>();
+        var strs = arr.Split(" ");
+
+        for (var i = 0; i < strs.Length; i++)
+        {
+            if (counter < k)
+            {
+                nums.Add(int.Parse(strs[i]));
+            }
+            else if(counter >= k)
+            {
+                result += $"{(float)nums.Sum() / k} ";
+                nums.Remove(nums[0]);
+                nums.Add(int.Parse(strs[i]));
+            }
+
+            counter++;
+        }
+
+        return result += $"{(float)nums.Sum() / k} ";
+    }
+}
