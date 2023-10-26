438d152394ec21f079cacbd91a5d934e27213b97
Новиков Никита <novikov.nik.job@yandex.ru>
2023-10-27 00:02:14 +0300

    Добавил онфиормацию о с#

---
+++ Backend/Practice/С#/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿// Введение, HelloWorld
+Console.WriteLine("Hello, World!");
+Console.Write("Congratulations!");
+Console.Write(" ");
+Console.Write("You wrote your first lines of code.\n");
+
+//Задача. Написание кода в редакторе .NET для отображения двух сообщений
+Console.WriteLine("This is the first line.");
+Console.WriteLine("This is the second line.");
+
+//Задача. Считать введенную строку и вывести ее в консоль
+Console.WriteLine("Введите свое имя: ");
+var name = Console.ReadLine();       // вводим имя
+Console.WriteLine($"Привет {name}");    // выводим имя на консоль
---
+++ Backend/Practice/С#/Zipper-Closure/ConsoleApp1/Program.cs
@@ -0,0 +1,87 @@
+﻿public class Programm
+{
+    public static TextReader reader = new StreamReader(Console.OpenStandardInput());
+    public static TextWriter writer = new StreamWriter(Console.OpenStandardOutput());
+
+    public static void Main(string[] args)
+    {
+        writer.WriteLine("Введите длинну массива");
+        var arrsLenght = ReadInteger();
+
+        writer.WriteLine("Введите значения первого массива");
+        var strNums = reader.ReadLine().Split(" ");
+        var firstArr = ReadArr(arrsLenght, strNums);
+
+        writer.WriteLine("Введите значения второго массива");
+        strNums = reader.ReadLine().Split(" ");
+        var secondArr = ReadArr(arrsLenght, strNums);
+
+        var result = GetZipperCloseArr(firstArr, secondArr);
+        var resultStr = "";
+        foreach (var item in result)
+        {
+            resultStr += item + " ";
+        }
+
+        writer.WriteLine(resultStr);
+
+        reader.Close();
+        writer.Close();
+    }
+
+    public static int[] GetZipperCloseArr(int[] first, int[] second)
+    {
+        var resultArrLenght = first.Length + second.Length;
+        var resultArr = new int[resultArrLenght];
+        var resArrCounter = 0;
+
+        for (int i = 0; i < resultArrLenght / 2; i++)
+        {
+            resultArr[resArrCounter] = first[i];
+            resArrCounter++;
+            resultArr[resArrCounter] = second[i];
+            resArrCounter++;
+        }
+
+        return resultArr;
+    }
+
+    public static int[] ReadArr(int arrLenght, string[] strNums)
+    {
+        var arr = new int[arrLenght];
+
+        if (strNums.Length != arrLenght)
+        {
+            writer.WriteLine("Длинна введенного  массива не совпадает с заданной изначально.");
+            throw new ArgumentOutOfRangeException("Длинна введенного  массива не совпадает с заданной изначально.");
+        }
+
+        for (int i = 0; i < arrLenght; i++)
+        {
+            if (int.TryParse(strNums[i], out var num))
+            {
+                arr[i] = num;
+            }
+            else
+            {
+                writer.WriteLine("Введенный аргумент имеет неверный формат.");
+                throw new ArgumentException("Введенный аргумент имеет неверный формат.");
+            }
+        }
+
+        return arr;
+    }
+
+    public static int ReadInteger()
+    {
+        if (int.TryParse(reader.ReadLine(), out var num))
+        {
+            return num;
+        }
+        else
+        {
+            writer.WriteLine("Введенный аргумент имеет неверный формат.");
+            throw new ArgumentException("Введенный аргумент имеет неверный формат.");
+        }
+    }
+}
